7bd6564e0e8fb4169c2dbde667dc38c42303504f
Rob Watson <rob.watson@me.com>
2022-07-20 20:49:01 +0100

    Add bootstrap file to phpunit

---
+++ tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * Require my custom autoloader.
+ * NB: Composer's vendor directory is installer in ../../vendor (i.e. a level above my autoloader)
+ */
+require dirname(__DIR__).'/vendor/autoloader.php';
