615f4bbd9150c2d18cbdf294f4e9ac83c06b168c
Rob Watson <rob.watson@me.com>
2022-07-23 21:49:10 +0100

    Update autoloader, add comment to gitignore

--- src/vendor/autoloader.php
+++ src/vendor/autoloader.php
@@ -1,13 +1,19 @@
 <?php
 
-spl_autoload_register(function($className) {
-    // Convert the class name from using forward-slashes to whatever the system directory separator
-    // is then just append .php and attempt to load the file starting in this directory
-    // (the vendor dir)
-    $classFilename = __DIR__ . '/' . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
+/**
+ * The autoloader presented here is bare-bones.  It offers very little protection if something
+ * goes wrong!   As per the PSR-4 autoloading standard, this function shouldn't throw any
+ * exceptions, it should fail silently.  PHP itself will then fail to find the class being
+ * requested which is where the debugging exercise can start.
+ * In a change to PSR-4, this function assumes any classes it's looking for start in this directory.
+ */
 
+spl_autoload_register(function($className) {
+    $classFilename = __DIR__ 
+        . DIRECTORY_SEPARATOR
+        . str_replace('\\', DIRECTORY_SEPARATOR, $className)
+        . '.php';
     if (file_exists($classFilename)) {
-        // Deliberately using require over include in case the file can't be opened
-        require $classFilename;
+        @include $classFilename;
     }
 });
