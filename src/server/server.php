472657add3486272ca5244095f3958abb5148fab
Rob Watson <rob.watson@me.com>
2022-07-25 09:01:53 +0100

    Remove request_received file as the function is small enough to fit straight into the server

--- src/server/server.php
+++ src/server/server.php
@@ -1,6 +1,11 @@
 <?php
 
+use EventService\Http\Controller\PingController;
+use EventService\Http\Controller\DebugController;
+use EventService\Http\Controller\NotFoundController;
+use EventService\Http\Controller\ReceiveEventController;
 use SourcePot\Bag\ReadOnlyBag;
+use SourcePot\Util\JSON;
 use Swoole\Http\Server;
 use Swoole\Http\Request;
 use Swoole\Http\Response;
@@ -8,13 +13,11 @@ use Swoole\Http\Response;
 // This is my own super-special autoloader function that registers itself
 require dirname(__DIR__).'/vendor/autoloader.php';
 
-require __DIR__.'/request_received.php';
-
 $config = new ReadOnlyBag(parse_ini_file(__DIR__.'/conf.ini'));
 
 $server = new Server($config->get('host'), $config->get('port'));
 
-$server->on("Start", function(Server $server) use($config) {
+$server->on("Start", static function(Server $server) use($config) {
     echo 'Swoole http server is started at http://'
         . $config->get('host')
         . ':'
@@ -22,18 +25,45 @@ $server->on("Start", function(Server $server) use($config) {
         . "\n";
 });
 
-$server->on("Request", function(Request $request, Response $response) use ($config) {
-    // try {
-        
-        requestReceived($request, $response);
-    // } catch (Throwable $t) {
-        // echo $t->getMessage();
-    // }
+$server->on("Request", static function(Request $request, Response $response) use ($config) {
+    $start = microtime(true);
+    echo "{$request->server['request_method']} request received!\n";
 
-    if($config->has('verbose')) {
-        echo "Memory used: " . number_format(memory_get_usage()/1024/1024, 2) . "M\n";
+    try {
+        // Handle json bodies and attach to Requests's post property (it'll be empty by default)
+        if ($request->header['contentType'] ?? '' === 'application/json') {
+            $request->post = SwoolePostDataTransformer::toArray($request->getContent());
+        }
+
+        $url = rtrim($request->server['request_uri'], '/');
+
+        $controller = match($url) {
+            '/ping' => (new PingController)->__invoke($request, $response),
+            '/debug' => (new DebugController)->__invoke($request, $response),
+            '/event' => (new ReceiveEventController)->__invoke($request, $response),
+            default => (new NotFoundController)->__invoke($request,$response)
+        };
     }
-    $response->end();
+    catch (\Throwable $t) {
+        // log the error
+        echo $t->getMessage();
+
+        // send the error back to the client
+        // @todo remove this eventually
+        $response->write($t->getMessage());
+    }
+    finally {
+        if($config->has('verbose')) {
+            $end = (microtime(true) - $start) * 1000;
+            echo 'Memory used: '
+                . number_format(memory_get_usage() / 1024 / 1024, 2)
+                . 'MB, time taken: '
+                . number_format($end, 4, '.', '')
+                . "ms\n";
+        }
+    
+        $response->end();
+    }
 });
 
 $server->start();
